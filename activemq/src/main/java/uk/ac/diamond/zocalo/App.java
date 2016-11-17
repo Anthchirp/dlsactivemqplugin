6fdf3fcc149c58fb48e62aed514fb706dc0b9935
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-17 15:01:37 +0000

    Initial commit

---
+++ activemq/src/main/java/uk/ac/diamond/zocalo/App.java
@@ -0,0 +1,13 @@
+package uk.ac.diamond.zocalo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
